2327c217e08775daae7dcf1f6425cd897a62f30c
Eleuterio Fulaho Notico <eleuterio3d@gmail.com>
2023-09-08 20:29:00 +0200

    FINALIZAR O PROJECTO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,38 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome")
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo ${conteudo.nome} adicionado à formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    val formacaoKotlin = Formacao("Formação Kotlin")
+    val formacaoJava = Formacao("Formação Java")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 30)
+    val conteudo2 = ConteudoEducacional("Kotlin Avançado", 45)
+    val conteudo3 = ConteudoEducacional("Java Básico", 40)
+
+    formacaoKotlin.adicionarConteudo(conteudo1)
+    formacaoKotlin.adicionarConteudo(conteudo2)
+    formacaoJava.adicionarConteudo(conteudo3)
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoJava.matricular(usuario2)
 }
